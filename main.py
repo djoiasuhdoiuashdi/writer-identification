a9a26bfeed6817b20fdb68afff4134b264ec8b6c
dom <dominic.akt@tuwien.ac.at>
2025-03-27 12:31:17 +0100

    refactor

--- main.py
+++ main.py
@@ -109,21 +109,21 @@ def main():
         resnet_output_path = os.path.join("resnet20_output", directory)
 
 
-        # if not os.path.exists("extract_patches_output"):
-        #     os.mkdir("extract_patches_output")
-        # if not os.path.exists(output_path):
-        #     os.mkdir(output_path)
-        # else:
-        #     shutil.rmtree(output_path)
-        #     os.mkdir(output_path)
-        # subprocess.run([sys.executable, 'extract_patches.py', "--in_dir", input_path, "--out_dir", output_path, "--num_of_clusters", f"{RESNET_NUM_CLUSTER}", "--centered", "True", "--black_pixel_thresh", f"{BLACK_PIXEL_THRESHOLD}", "--white_pixel_thresh", f"{WHITE_PIXEL_THRESHOLD}", "--scale", "1"]
-        #                        , stdout=None, stderr=None)
-        # center_path = os.path.join(output_path, 'centers.pkl')
-        # parameter_path = os.path.join(output_path, 'db-creation-parameters.json')
-        # if os.path.exists(center_path):
-        #     os.remove(center_path)
-        # if os.path.exists(parameter_path):
-        #     os.remove(parameter_path)
+        if not os.path.exists("extract_patches_output"):
+            os.mkdir("extract_patches_output")
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+        else:
+            shutil.rmtree(output_path)
+            os.mkdir(output_path)
+        subprocess.run([sys.executable, 'extract_patches.py', "--in_dir", input_path, "--out_dir", output_path, "--num_of_clusters", f"{RESNET_NUM_CLUSTER}", "--centered", "True", "--black_pixel_thresh", f"{BLACK_PIXEL_THRESHOLD}", "--white_pixel_thresh", f"{WHITE_PIXEL_THRESHOLD}", "--scale", "1"]
+                               , stdout=None, stderr=None)
+        center_path = os.path.join(output_path, 'centers.pkl')
+        parameter_path = os.path.join(output_path, 'db-creation-parameters.json')
+        if os.path.exists(center_path):
+            os.remove(center_path)
+        if os.path.exists(parameter_path):
+            os.remove(parameter_path)
 
         # ---------------------------------------------------------------------------------------------------
         # TRAIN RESNET
@@ -135,17 +135,17 @@ def main():
             os.mkdir(resnet_output_path)
 
 
-        # subprocess.run([sys.executable, 'train_resnet20.py',
-        #     "--arch", "resnet20",
-        #     "--workers", "8",
-        #     "--epochs", "200",
-        #     "--batch-size", "32",
-        #     "--lr", "0.01",
-        #     "--momentum", "0.95",
-        #     "--weight-decay", "0.00065",
-        #     "--output_dir", resnet_output_path,
-        #     "--input_dir", output_path],
-        #     stdout=None, stderr=None)
+        subprocess.run([sys.executable, 'train_resnet20.py',
+            "--arch", "resnet20",
+            "--workers", "8",
+            "--epochs", "200",
+            "--batch-size", "32",
+            "--lr", "0.01",
+            "--momentum", "0.95",
+            "--weight-decay", "0.00065",
+            "--output_dir", resnet_output_path,
+            "--input_dir", output_path],
+            stdout=None, stderr=None)
 
         # ---------------------------------------------------------------------------------------------------
         # TRAIN VLAD
